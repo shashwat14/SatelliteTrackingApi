af9caefb97c12bfe38a777bd218d1eb016dadbca
Shashwat Verma <shashwatverma14@gmail.com>
2017-04-30 10:34:14 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class Satellite(object):
         return json
         
     def getLocation(self):
-        data = (self.fetch()).json()[0]['pos'][-1]['d'].split('|')
+        data = (self.fetch()).json()[0]['pos'][0]['d'].split('|')
         return data
         
     def setLocation(self, data):
@@ -53,3 +53,19 @@ class Satellites(object):
             KVP = sat.getKVP()
             dic[KVP['name']] = KVP
         return json.dumps(dic)
+        
+aqua = Satellite('http://www.n2yo.com/sat/instant-tracking.php?s=27424&hlat=1.28967&hlng=103.85007&d=300&r=588399124880.8425&tz=GMT+05:30&O=n2yocom&rnd_str=b5ec47c4e4e4721c2e0fa5f3a681e221&callback=', 'aqua', 1690000.)
+calipso = Satellite('http://www.n2yo.com/sat/instant-tracking.php?s=29108&hlat=1.28967&hlng=103.85007&d=300&r=1173810753651.1362&tz=GMT+05:30&O=n2yocom&rnd_str=b5ec47c4e4e4721c2e0fa5f3a681e221&callback=', 'callipso', 92.)
+cloudsat = Satellite('http://www.n2yo.com/sat/instant-tracking.php?s=29107&hlat=1.28967&hlng=103.85007&d=300&r=1027239537293.5146&tz=GMT+05:30&O=n2yocom&rnd_str=b5ec47c4e4e4721c2e0fa5f3a681e221&callback=', 'cloudsat', 1400.)
+while True:
+    print ("Fetching data at " + str(time.time()))
+    f = open('data.txt', 'w')
+    aqua.setLocation(aqua.getLocation())
+    calipso.setLocation(calipso.getLocation())
+    cloudsat.setLocation(cloudsat.getLocation())
+    lst = [aqua, calipso, cloudsat]
+    s = Satellites(lst)
+    f.write(s.getJson())
+    f.close()
+    time.sleep(1)
+    
